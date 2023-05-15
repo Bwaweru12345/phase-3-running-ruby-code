6c7383741db2aaaa1db3d7811a2b8c09300f1c08
Bwaweru12345 <bryanwaweru1076@example.com>
2023-05-15 16:12:16 +0300

    Complete assignment

--- app.rb
+++ app.rb
@@ -14,10 +14,26 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+# def add num1, num2
+#     puts num1
+#     puts num2
+#     num1 + num2
+# end
 
-puts add(1,3) + add(2,2)
+# puts add(1,3) + add(2,2)
+
+
+# # print "Hello world!"
+# # print "Hello world!"
+# # print"Hello world!"
+# # puts [1, 2, 3]
+# p [1, 2, 3]
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+
+
+
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
